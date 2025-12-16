e160c1ce924e53eeb17c75e85821e8ec88f8a58e
s1f10240251 <s1f102402518@iniad.org>
2025-12-16 17:20:36 +0900

    psqlに変更

--- config/settings.py
+++ config/settings.py
@@ -74,15 +74,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'enshu4',
+        'USER': 'iniad',
+        'PASSWORD': 'vUGoQ2en1u',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
