24dfd785188f809e453d5e46de34f70b3a6422e6
s1f10240251 <s1f102402518@iniad.org>
2026-05-01 23:31:15 +0900

    asgi内バグ修正

--- config/asgi.py
+++ config/asgi.py
@@ -7,21 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
+from django.core.asgi import get_asgi_application
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
 
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import App.routing
+from importlib import import_module
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            App.routing.websocket_urlpatterns
-        )
+        URLRouter(import_module("App.routing").websocket_urlpatterns)
     ),
 })
-
